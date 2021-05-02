4ee7fecc8be37e2539bb525c8c685f837a27a34a
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2021-05-02 09:25:15 +0200

    Initial commit

---
+++ src/main/java/pl/baadamczyk/wafflewithit/WaffleWithItApplication.java
@@ -0,0 +1,13 @@
+package pl.baadamczyk.wafflewithit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaffleWithItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaffleWithItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/baadamczyk/wafflewithit/WaffleWithItApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.baadamczyk.wafflewithit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaffleWithItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
